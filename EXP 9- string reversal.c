1750c5b54daccc39baa615bcd18fbee4bec589da
Divvye Kansara <divvyesk2428@gmail.com>
2025-02-10 20:12:38 +0530

    Add files via upload

---
+++ EXP 9- string reversal.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#define SIZE 5
+#include<stdbool.h>
+
+int s[SIZE]= {};
+int arr[SIZE]= {};
+int top = -1;
+
+bool isEmpty()
+{
+	if(top == -1)
+	return true;
+	
+	else
+	return false;
+}
+
+bool isFull()
+{
+		if(top == SIZE-1)
+		return true;
+		
+		else
+		return false;
+}
+
+int pop()
+{
+		if(isEmpty())
+		{
+			printf("UNDERFLOW!");
+			return 0;
+		}
+		
+		else
+		{
+		int val= s[top];
+		top--;
+		return val;
+		}
+}
+
+void push(int element)
+{
+		if(isFull())
+		printf("OVERFLOW!");
+		
+		else
+		{
+			top++;
+			s[top]= element;
+		}
+}
+
+
+int main()
+{
+	
+	
+	
+	printf("Enter a string of integers: ");
+	for(int i=0; i<SIZE; i++)
+	{
+		scanf("%d", &arr[i]);
+		push(arr[i]);
+	}
+	
+	for(int i=0; i<SIZE; i++)
+	{
+		arr[i]= pop();
+		printf("%d ", arr[i]);
+	}
+	
+	
+	
+	return 0;
+	
+}
