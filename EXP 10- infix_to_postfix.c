e0bf0a50d80000fb76358ff6925e6a80e8752a34
Divvye Kansara <divvyesk2428@gmail.com>
2025-02-19 14:04:00 +0530

    Update EXP 10- infix_to_postfix.c

--- EXP 10- infix_to_postfix.c
+++ EXP 10- infix_to_postfix.c
@@ -4,8 +4,8 @@
 #include<string.h>
 #include<stdbool.h>
 
-char s[MAX];
-char op[MAX];
+char s[MAX]; //to store the operators 
+char op[MAX]; //to store the output postfix expression
 int top = -1;
 
 bool isEmpty()
@@ -87,7 +87,7 @@ int main()
 	
 	for(int i=0; i<lenin; i++)
 	{
-		//case1: if it is an operand
+		//case1: if it is an operand, append it to the output string
 		if(isalnum(ip[i]))
 		{
 			int len= strlen(op);
@@ -95,13 +95,13 @@ int main()
 			op[len+1]= '\0';
 		}
 		
-		//case2: if opening parenthesis
+		//case2: if opening parenthesis, push it into the stack
 		else if(ip[i] == '(')
 		{
 			push(ip[i]);
 		}
 		
-		//case3: if closing parenthesis
+		//case3: if closing parenthesis, pop all the operators in the stack till ( is encountered
 		else if(ip[i] == ')')
 		{
 			while((!isEmpty()) && (s[top] != '(')) //op main s[top] concat
@@ -115,10 +115,11 @@ int main()
 			pop(); //to discard the leftover ( 
 		}
 		
-		//case4: if it is an operator
+		//case4: if it is an operator, it will be pushed into the stack if it has higher precedence than s[top] and 
+		//if it doesn't it will be appended in the output expression till we find a (
 		else
 		{
-			while((!isEmpty()) && (s[top] != '(') && (s[top] >= precedence(ip[i])))
+			while((!isEmpty()) && (s[top] != '(') && (precedence(s[top]) >= precedence(ip[i])))
 			{
 				int len= strlen(op);
 				op[len]=s[top];
@@ -126,11 +127,13 @@ int main()
 				pop();
 			}
 			
-			push(ip[i]);
+			push(ip[i]); //incase any of the while loop condition becomes false, the operator is pushed in
+			//the stack (ex. precedence of ip[i] was greater than s[top])
 		}
 		
 	}
-	
+
+	//incase the expression is over but there is still something left in the stack, append all of it in the output expression
 	while(!isEmpty())
 	{
 		int len= strlen(op);
